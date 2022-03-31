67160ff026dbbb594ca4f74ef2172926e4845566
negiryu <ryu-negishi@higuchi-consulting.com>
2022-03-31 10:44:45 +0900

    IfQuestion

---
+++ rsc/QuestionIF.java
@@ -0,0 +1,17 @@
+package rsc;
+
+public class QuestionIF {
+    public static void main(String[] args) {
+        boolean result = false;
+
+        if (true) {
+            if (result) {
+                System.out.println("結果はTRUEでした");
+            } else {
+                System.out.println("結果はFALSEでした");
+            }
+        }
+
+        System.out.println("処理を終了します");
+    }
+}
